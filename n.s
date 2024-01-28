50b2aa781ef06201958fa23cbf150b4bc768a016
Dion Lucca <120736459+odjion@users.noreply.github.com>
2024-01-28 00:26:21 -0300

    Create n.s

---
+++ n.s
@@ -0,0 +1,19 @@
+ alert('Boas-vindas ao jogo do número secreto')
+ let numeroSecreto = 4;
+ let chute;
+ let tentativas = 1;
+ console.log(numeroSecreto)
+ let chute = prompt('Escolha um número entre 1 e 10')
+
+// enquanto o chute não for igual ao n.s
+ while (chute != numeroSecreto) {
+ chute = prompt ('Escolha https://github.com/odjion/curso.alura-ONE-NEXT-EDUCATION/tree/mainum número entre 1 e 10')
+
+// se o chute for igual ao número secreto
+
+ if (chute == numeroSecreto) {
+    alert('Isso aí! Você descobriu o número secreto $ {numeroSecreto)
+');
+ } else {
+alert (Você errou: )
+}
